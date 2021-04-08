c9fcf5adb1a1bccfe25f290fdfdcc4a1db26eba5
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-08 12:57:35 -0400

    setup middleware

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("cors");
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/",(_, res)=>{
+    res.json({data: "API is accounted for"});
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening For Port On ${port}`)
+});
+
+// console.log("ITS ALIVE!");
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USERNAME);
+// console.log(process.env.PORT);
+// console.log(process.env.FOOD);
+// console.log(process.env.REALLY);
+// console.log(process.env.GREET);
